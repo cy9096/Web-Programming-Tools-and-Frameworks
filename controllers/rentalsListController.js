f3b48e72f46680366d102f573d9ca514adafff04
taicelaw@gamil.com <cluo21@myseneca.ca>
2023-03-08 19:54:00 -0500

    comment rentalsListController.js
    code doesn't work

--- controllers/rentalsListController.js
+++ controllers/rentalsListController.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const router = express.Router();
-const rentals = require("../models/rentalList");
+// const express = require("express");
+// const router = express.Router();
+// const rentals = require("../models/rentalList");
 
-router.get("/rentals", (req, res) => {
-    res.render("general/rentals", {
-        rentalsData: rentals.getRentalsByCityAndProvince()
-    });
-});
+// router.get("/rentals", (req, res) => {
+//     res.render("general/rentals", {
+//         rentalsData: rentals.getRentalsByCityAndProvince()
+//     });
+// });
 
-router.get("/", (req, res) => {
-    res.render("general/home", {
-        rentalsData: rentals.getFeaturedRentals()
-    })
-});
+// router.get("/", (req, res) => {
+//     res.render("general/home", {
+//         rentalsData: rentals.getFeaturedRentals()
+//     })
+// });
 
 
-module.exports = router;
+// module.exports = router;
