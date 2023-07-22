d4ff588db529c789fe3b0c8f271c76b6243c2beb
Wing <cluo21@myseneca.ca>
2023-07-22 02:25:27 -0400

    change the condition for data-clerk

--- controllers/load-data.js
+++ controllers/load-data.js
@@ -1,39 +1,46 @@
+
 const express = require("express");
 const router = express.Router();
 const rentals = require("../models/rentals-db");
-//const userModel = require("../models/userModel");
 const rentalModel = require("../models/rentalModel");
-//const mongoose = require("mongoose");
-//const bcrypt = require("bcryptjs");
+
 
 router.get("/rentals", (req, res) => {
     // protect the route, so only the data clerk can access it
-    if (req.session.isClerk) {
-        const message = {};
+    if (req.session.isClerk === true) {
+      const message = {};
 
-        rentalModel.count().then((count) => {
-            if (count === 0) {
-                // load the data
-                rentalModel.insertMany(rentals.getAllRentals())
-                    .then(() => {
-                        res.render("load-data/rentals", {
-                            message: "Successfully loaded data! Added rentals to the database"
-                        });
-                    })
-                    .catch((err) => {
-                        res.send("Could not insert documents into the database: " + err);
-                    });
-            } else {
+      rentalModel
+        .count()
+        .then((count) => {
+          if (count === 0) {
+            // if there are no rentals in the database, insert them
+            rentalModel
+              .insertMany(rentals.getAllRentals())
+              .then(() => {
                 res.render("load-data/rentals", {
-                    message: "Data already loaded. Rentals already in the database"
+                  message:
+                    "Successfully loaded data! Added rentals to the database",
                 });
-            }
-        }).catch((err) => {
-            res.send("Error checking rental count: " + err);
+              })
+              .catch((err) => {
+                res.send(
+                  "Could not insert documents into the database: " + err
+                );
+              });
+          } else {
+            // if there are rentals in the database, don't insert them
+            res.render("load-data/rentals", {
+              message: "Data already loaded. Rentals already in the database",
+            });
+          }
+        })
+        .catch((err) => {
+          res.send("Error checking rental count: " + err);
         });
     } else {
-        // someone else is signed in, so they can't access the data
-        res.status(401).send("You are not authorized to access this page");
+      // someone else is signed in, so they can't access the data
+      res.status(401).send("You are not authorized to access this page");
     }
 });
 /*===========================================================================================*/
